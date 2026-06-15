8fee224d070a1846096dd779149d81e9ba6d28b2
suntech\c23009 <c23009@suntech.jp>
2026-06-15 15:26:56 +0900

    バグの修正をしました#4

--- Sample.java
+++ Sample.java
@@ -2,6 +2,11 @@ public class Sample {
     public static void main(String[] args) {
         int a = 10;
         int b = 0;
-        System.out.println(a/b);
+
+        try {
+            System.out.println(a/b);
+        } catch (ArithmeticException e) {
+            System.out.println("0で割ることはできません。");
+        }
     }
 }
